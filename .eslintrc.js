51dbaa28cb6d60c0dd6931d78c202ba78e84a98a
antonio <antonio@pykiss.com>
2020-11-02 17:01:15 +0100

    working in the arabic to roman function, recursive

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'no-plusplus': 0,
   },
 };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 function validateArabic(number) {
   if (number < 1 || number > 3999) {
     throw new Error('invalid range');
@@ -17,11 +18,40 @@ function validateRoman(string) {
   }
 }
 
+const pieces = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I'],
+];
+
 function romanToArabic(roman) {
 
 }
 
-function arabicToRoman(arabic) {
+function arabicToRoman(arabic, current = '') {
+  let i = 0;
+  while (arabic < pieces[i][0]) {
+    i++;
+  }
+  if (arabic % pieces[i][0] === 0) {
+    return current + pieces[i][1].repeat(arabic / pieces[i][1])
+  }
+
+
+
+  current = current + pieces[i][1].repeat(arabic / pieces[i][1])
+
+  return arabicToRoman(arabic - )
 
 }
 
